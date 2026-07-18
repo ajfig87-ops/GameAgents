67434398d4940a387f0ed2a22ed9ec658b1140f5
Alex Figueroa <ajfig87@gmail.com>
2026-07-18 09:49:34 -0400

    Switch to pydirectinput for reliable input

--- agents/games/csgo/input_control.py
+++ agents/games/csgo/input_control.py
@@ -1,17 +1,20 @@
-import subprocess
+import pydirectinput
 import time
 
 class InputControl:
+    def __init__(self):
+        pydirectinput.FAILSAFE = False
+    
     def move_mouse(self, dx, dy):
-        subprocess.run(["xdotool", "mousemove_relative", str(dx), str(dy)])
+        pydirectinput.moveRel(dx, dy)
     
     def click(self, button="left"):
-        subprocess.run(["xdotool", "click", "1" if button == "left" else "3"])
-    
-    def press_key(self, key):
-        subprocess.run(["xdotool", "key", key])
+        if button == "left":
+            pydirectinput.leftClick()
+        else:
+            pydirectinput.rightClick()
     
     def hold_key(self, key, duration=0.1):
-        subprocess.run(["xdotool", "keydown", key])
+        pydirectinput.keyDown(key)
         time.sleep(duration)
-        subprocess.run(["xdotool", "keyup", key])
+        pydirectinput.keyUp(key)
