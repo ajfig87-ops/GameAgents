1631af88de58e24f6056d905598a3341867f8bd9
Alex Figueroa <ajfig87@gmail.com>
2026-07-18 09:51:46 -0400

    Fix input control with xdotool

--- agents/games/csgo/input_control.py
+++ agents/games/csgo/input_control.py
@@ -1,20 +1,23 @@
-import pydirectinput
+import subprocess
 import time
 
 class InputControl:
     def __init__(self):
-        pydirectinput.FAILSAFE = False
+        self.window_name = "Counter-Strike: Global Offensive"
+    
+    def focus_window(self):
+        subprocess.run(["xdotool", "search", "--name", self.window_name, "windowactivate"], stderr=subprocess.DEVNULL)
     
     def move_mouse(self, dx, dy):
-        pydirectinput.moveRel(dx, dy)
+        self.focus_window()
+        subprocess.run(["xdotool", "mousemove_relative", str(dx), str(dy)])
     
     def click(self, button="left"):
-        if button == "left":
-            pydirectinput.leftClick()
-        else:
-            pydirectinput.rightClick()
+        self.focus_window()
+        subprocess.run(["xdotool", "click", "1" if button == "left" else "3"])
     
     def hold_key(self, key, duration=0.1):
-        pydirectinput.keyDown(key)
+        self.focus_window()
+        subprocess.run(["xdotool", "keydown", key])
         time.sleep(duration)
-        pydirectinput.keyUp(key)
+        subprocess.run(["xdotool", "keyup", key])
